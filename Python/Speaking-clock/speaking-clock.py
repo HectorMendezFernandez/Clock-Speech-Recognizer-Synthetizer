ef1c179f62bf16a55a9b5d972512bafa81826caf
HectorMendezCIP <hector.mendez.fernandez@est.una.ac.cr>
2024-11-04 21:43:56 -0600

    Add base code - Configure speech service

---
+++ Python/Speaking-clock/speaking-clock.py
@@ -0,0 +1,58 @@
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+import azure.cognitiveservices.speech as speech_sdk
+
+
+def main():
+    try:
+        global speech_config
+
+        # Get Configuration Settings
+        load_dotenv()
+        ai_key = os.getenv('SPEECH_KEY')
+        ai_region = os.getenv('SPEECH_REGION')
+
+        # Configure speech service
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
+        print('Ready to use speech service in:', speech_config.region)
+        
+
+        # Get spoken input
+        command = TranscribeCommand()
+        if command.lower() == 'what time is it?':
+            TellTime()
+
+    except Exception as ex:
+        print(ex)
+
+def TranscribeCommand():
+    command = ''
+
+    # Configure speech recognition
+
+
+    # Process speech input
+
+
+    # Return the command
+    return command
+
+
+def TellTime():
+    now = datetime.now()
+    response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
+
+
+    # Configure speech synthesis
+    
+
+    # Synthesize spoken output
+
+
+    # Print the response
+    print(response_text)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Clock-Speech-Recognizer-Synthetizer
 An app that use Azure AI Voice services to create a speaking clock application
+
+Steps: 
+1. Configure the app:  pip install azure-cognitiveservices-speech==1.30.0
+
